cb8997d5ee7c4179da7858a6768f5162c2de2966
ajbater <amanda22@hotmail.co.uk>
2017-01-26 13:30:50 +0000

    game has no players when it is first created

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,11 @@
 class Game
 
+  attr_reader :board
+  attr_accessor :player1
+
+  def initialize(board = board_klass.new)
+    @board = board
+    @player1 = nil
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,20 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  let(:board) { double :board }
+  let(:player1) { double :player }
+  subject(:game) { described_class.new(board) }
 
-  
+  it 'takes a board upon instantiation' do
+    expect(game.board).to eq board
+  end
+
+  it 'has no players when a new game is created' do
+    expect(game.player1).to eq nil
+  end
+
+  # it 'can take players' do
+  #   game.add_player(player1)
+  #   expect(game.player1).to eq player1
+  # end
 end
